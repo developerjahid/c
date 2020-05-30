36ae6def4277ebc760759014c11ca767a71d9234
Developer Jahid <jhpabel@gmail.com>
2020-05-30 20:51:54 +0600

    hello world & variables

---
+++ c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    //hello world
+    printf("Hello World\n");
+    //variables
+    int age = 44;
+    printf("my age is %d\n", age);
+    printf("minhajul age is %d\n", age);
+    printf("hi, i am %d\n", age);
+
+    return 0;
+}
